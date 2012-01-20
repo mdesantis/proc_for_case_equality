00d9b806322bbb57f5ee0475c9f3ae1c0a253f89
Maurizio De Santis <desantis.maurizio@gmail.com>
2012-01-20 16:33:02 +0100

    added documentation

--- lib/proc_for_case_equality.rb
+++ lib/proc_for_case_equality.rb
@@ -1,4 +1,11 @@
+# This class inherits from +Proc+ and overrides its +===+ method
+# letting you use proc instances in +case+ statements (see the {file:README.rdoc README})
 class ProcForCaseEquality < Proc
+  # It calls +Proc#call+ in order to get used together in
+  # the +case+ statement.
+  #
+  # @param *params Any object.
+  # @return the proc calling using its argument(s) as argument(s)
   def ===(*params)
     self.call *params
   end
--- lib/proc_for_case_equality/pfce.rb
+++ lib/proc_for_case_equality/pfce.rb
@@ -1,2 +1,4 @@
 require 'proc_for_case_equality'
+
+# Alias of {ProcForCaseEquality}.
 PFCE = ProcForCaseEquality
